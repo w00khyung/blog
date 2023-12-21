084de7fc573b07a3ca80e69f1371e4641a65737f
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-21 14:40:39 +0900

    ✨ gtag 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,6 +55,10 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-9FH5ZSHSMG',
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
