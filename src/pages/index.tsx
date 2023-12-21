27e1195ae44acf9ff40465c835f9fe7fc6e75d44
HyungWookChoi <qpflapffhs76@gmail.com>
2023-12-21 13:34:18 +0900

    ✏️ 링크 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         </Heading>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className='button button--secondary button--lg' to='/dev'>
+          <Link className='button button--secondary button--lg' to='/docs/study/introduction'>
             살펴보기
           </Link>
         </div>
